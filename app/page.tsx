86f21cef86e479593e834d45acf840bb23ccd1cd
Ankush Agarwal <ankushagarwal11@gmail.com>
2024-12-22 20:21:06 -0800

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,25 @@ import Chat, { ChatHandle } from "@/components/chat";
 import { useRef, useState } from "react";
 
 export default function Home() {
-  const submitRef = useRef<ChatHandle>(null);
+  const submitRef1 = useRef<ChatHandle>(null);
+  const submitRef2 = useRef<ChatHandle>(null);
+  const submitRef3 = useRef<ChatHandle>(null);
   const [inputValue, setInputValue] = useState("");
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     // Check if the Command (meta) key and Enter key are pressed
     if (event.metaKey && event.key === "Enter") {
-      if (submitRef.current) {
-        console.log("Calling submit from parent");
-        submitRef.current.submit();
+      if (submitRef1.current) {
+        console.log("Calling submit1 from parent");
+        submitRef1.current.submit();
+      }
+      if (submitRef2.current) {
+        console.log("Calling submit2 from parent");
+        submitRef2.current.submit();
+      }
+      if (submitRef3.current) {
+        console.log("Calling submit3 from parent");
+        submitRef3.current.submit();
       }
     }
   };
@@ -25,21 +35,21 @@ export default function Home() {
       <div className="flex flex-1 overflow-auto">
         <div className="flex-1 border-2 h-fullrounded overflow-scroll">
           <Chat
-            ref={submitRef}
+            ref={submitRef1}
             inputValue={inputValue}
             modelName="anthropic/claude-3.5-sonnet:beta"
           />
         </div>
         <div className="flex-1 border-2  rounded overflow-scroll">
           <Chat
-            ref={submitRef}
+            ref={submitRef2}
             inputValue={inputValue}
             modelName="openai/gpt-4o-mini"
           />
         </div>
         <div className="flex-1 border-2  rounded overflow-scroll">
           <Chat
-            ref={submitRef}
+            ref={submitRef3}
             inputValue={inputValue}
             modelName="google/gemini-flash-1.5"
           />
