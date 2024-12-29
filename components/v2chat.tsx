a68cdde0cf8321d588676380e0f03f743d1db208
Ankush Agarwal <ankushagarwal11@gmail.com>
2024-12-28 19:41:45 -0800

    Add User and AI Messages to v2chat

--- components/v2chat.tsx
+++ components/v2chat.tsx
@@ -8,12 +8,44 @@ export default function V2Chat() {
       <div className="w-full h-full rounded-md border border-gray-alpha-400">
         <div className="h-full w-full overflow-hidden rounded-md">
           <div
-            id="scroll-container-3668f080-4099-4337-9b99-bfac1df9abf9_1"
             className="flex flex-col flex-no-wrap h-full overflow-y-auto overscroll-y-none"
             style={{ overflowAnchor: 'none' }}
           >
             <div className="sticky top-0 z-10 flex-shrink-0 min-w-0 min-h-0 border-b">
-              <div className="flex items-center bg-background-200 backdrop-blur shadow-[0_1px_rgba(202,206,214,.3),0_5px_10px_-5px_rgba(0,0,0,.05)] dark:shadow-[0_1px_rgba(255,255,255,0.15)] justify-between py-3 pl-3 pr-2"></div>
+              <div className="flex items-center bg-background-200 backdrop-blur shadow-[0_1px_rgba(202,206,214,.3),0_5px_10px_-5px_rgba(0,0,0,.05)] dark:shadow-[0_1px_rgba(255,255,255,0.15)] justify-between py-3 pl-3 pr-2">
+                <div className="flex items-center">Model Name</div>
+              </div>
+            </div>
+
+            <div className="flex-1 min-w-0">
+              <div className="scrolling-touch scrolling-gpu h-full w-full relative overflow-auto overscroll-y-auto">
+                <div className="h-full divide-y pb-12">
+                  {/* User message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message bg-zinc-100 dark:bg-zinc-900">
+                    <p>Hello User Message</p>
+                  </div>
+                  {/* AI message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message">
+                    <p>Hello AI Message</p>
+                  </div>
+                  {/* User message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message bg-zinc-100 dark:bg-zinc-900">
+                    <p>Hello User Message</p>
+                  </div>
+                  {/* AI message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message">
+                    <p>Hello AI Message</p>
+                  </div>
+                  {/* User message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message bg-zinc-100 dark:bg-zinc-900">
+                    <p>Hello User Message</p>
+                  </div>
+                  {/* AI message */}
+                  <div className="px-3 @md:py-4 py-2.5 group transition-opacity message">
+                    <p>Hello AI Message</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
