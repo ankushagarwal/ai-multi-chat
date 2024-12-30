889feedaf4154fbab3dd1cfc5be4027ac3feba0a
Ankush Agarwal <ankushagarwal11@gmail.com>
2024-12-29 17:21:06 -0800

    Add memoization and see if that helps

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -5,116 +5,170 @@ import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
+// @ts-expect-error
+const CodeBlockComponent = ({ children, className, ...rest }) => {
+  const match = /language-(\w+)/.exec(className || '');
+  return match ? (
+    <SyntaxHighlighter
+      {...rest}
+      PreTag="div"
+      // eslint-disable-next-line
+      children={String(children).replace(/\n$/, '')}
+      language={match[1]}
+      style={oneDark}
+      customStyle={{
+        borderRadius: '0.5rem',
+      }}
+    />
+  ) : (
+    <code {...rest} className={`${className} bg-zinc-200 px-1 rounded-sm`}>
+      {children}
+    </code>
+  );
+};
+const CodeBlock = memo(CodeBlockComponent);
+CodeBlock.displayName = 'CodeBlock';
+
+// @ts-expect-error
+const PreComponent = ({ children }) => <>{children}</>;
+const Pre = memo(PreComponent);
+Pre.displayName = 'Pre';
+
+// @ts-expect-error
+const OlComponent = ({ node, children, ...props }) => (
+  <ol className="list-decimal list-outside ml-4" {...props}>
+    {children}
+  </ol>
+);
+const Ol = memo(OlComponent);
+Ol.displayName = 'Ol';
+
+// @ts-expect-error
+const LiComponent = ({ node, children, ...props }) => (
+  <li className="py-1" {...props}>
+    {children}
+  </li>
+);
+const Li = memo(LiComponent);
+Li.displayName = 'Li';
+
+// @ts-expect-error
+const UlComponent = ({ node, children, ...props }) => (
+  <ul className="list-decimal list-outside ml-4" {...props}>
+    {children}
+  </ul>
+);
+const Ul = memo(UlComponent);
+Ul.displayName = 'Ul';
+
+// @ts-expect-error
+const StrongComponent = ({ node, children, ...props }) => (
+  <span className="font-semibold" {...props}>
+    {children}
+  </span>
+);
+const Strong = memo(StrongComponent);
+Strong.displayName = 'Strong';
+
+// @ts-expect-error
+const AComponent = ({ node, children, ...props }) => (
+  // @ts-expect-error
+  <Link
+    className="text-blue-500 hover:underline"
+    target="_blank"
+    rel="noreferrer"
+    {...props}
+  >
+    {children}
+  </Link>
+);
+const A = memo(AComponent);
+A.displayName = 'A';
+
+// @ts-expect-error
+const H1Component = ({ node, children, ...props }) => (
+  <h1 className="text-3xl font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h1>
+);
+const H1 = memo(H1Component);
+H1.displayName = 'H1';
+
+// @ts-expect-error
+const H2Component = ({ node, children, ...props }) => (
+  <h2 className="text-2xl font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h2>
+);
+const H2 = memo(H2Component);
+H2.displayName = 'H2';
+
+// @ts-expect-error
+const H3Component = ({ node, children, ...props }) => (
+  <h3 className="text-xl font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h3>
+);
+const H3 = memo(H3Component);
+H3.displayName = 'H3';
+
+// @ts-expect-error
+const H4Component = ({ node, children, ...props }) => (
+  <h4 className="text-lg font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h4>
+);
+const H4 = memo(H4Component);
+H4.displayName = 'H4';
+
+// @ts-expect-error
+const H5Component = ({ node, children, ...props }) => (
+  <h5 className="text-base font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h5>
+);
+const H5 = memo(H5Component);
+H5.displayName = 'H5';
+
+// @ts-expect-error
+const H6Component = ({ node, children, ...props }) => (
+  <h6 className="text-sm font-semibold mt-6 mb-2" {...props}>
+    {children}
+  </h6>
+);
+const H6 = memo(H6Component);
+H6.displayName = 'H6';
+
 const components: Partial<Components> = {
   // code: CodeBlock,
   // https://github.com/remarkjs/react-markdown?tab=readme-ov-file#use-custom-components-syntax-highlight
   // https://github.com/react-syntax-highlighter/react-syntax-highlighter
-  code: (props) => {
-    const { children, className, node, ...rest } = props;
-    const match = /language-(\w+)/.exec(className || '');
-    return match ? (
-      // @ts-expect-error
-      <SyntaxHighlighter
-        {...rest}
-        PreTag="div"
-        // eslint-disable-next-line
-        children={String(children).replace(/\n$/, '')}
-        language={match[1]}
-        style={oneDark}
-        customStyle={{
-          borderRadius: '0.5rem',
-        }}
-      />
-    ) : (
-      <code {...rest} className={`${className} bg-zinc-200 px-1 rounded-sm`}>
-        {children}
-      </code>
-    );
-  },
-  pre: ({ children }) => <>{children}</>,
-  ol: ({ node, children, ...props }) => {
-    return (
-      <ol className="list-decimal list-outside ml-4" {...props}>
-        {children}
-      </ol>
-    );
-  },
-  li: ({ node, children, ...props }) => {
-    return (
-      <li className="py-1" {...props}>
-        {children}
-      </li>
-    );
-  },
-  ul: ({ node, children, ...props }) => {
-    return (
-      <ul className="list-decimal list-outside ml-4" {...props}>
-        {children}
-      </ul>
-    );
-  },
-  strong: ({ node, children, ...props }) => {
-    return (
-      <span className="font-semibold" {...props}>
-        {children}
-      </span>
-    );
-  },
-  a: ({ node, children, ...props }) => {
-    return (
-      // @ts-expect-error
-      <Link
-        className="text-blue-500 hover:underline"
-        target="_blank"
-        rel="noreferrer"
-        {...props}
-      >
-        {children}
-      </Link>
-    );
-  },
-  h1: ({ node, children, ...props }) => {
-    return (
-      <h1 className="text-3xl font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h1>
-    );
-  },
-  h2: ({ node, children, ...props }) => {
-    return (
-      <h2 className="text-2xl font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h2>
-    );
-  },
-  h3: ({ node, children, ...props }) => {
-    return (
-      <h3 className="text-xl font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h3>
-    );
-  },
-  h4: ({ node, children, ...props }) => {
-    return (
-      <h4 className="text-lg font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h4>
-    );
-  },
-  h5: ({ node, children, ...props }) => {
-    return (
-      <h5 className="text-base font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h5>
-    );
-  },
-  h6: ({ node, children, ...props }) => {
-    return (
-      <h6 className="text-sm font-semibold mt-6 mb-2" {...props}>
-        {children}
-      </h6>
-    );
-  },
+  // @ts-expect-error
+  code: CodeBlock,
+  // @ts-expect-error
+  pre: Pre,
+  // @ts-expect-error
+  ol: Ol,
+  // @ts-expect-error
+  li: Li,
+  // @ts-expect-error
+  ul: Ul,
+  // @ts-expect-error
+  strong: Strong,
+  // @ts-expect-error
+  a: A,
+  // @ts-expect-error
+  h1: H1,
+  // @ts-expect-error
+  h2: H2,
+  // @ts-expect-error
+  h3: H3,
+  // @ts-expect-error
+  h4: H4,
+  // @ts-expect-error
+  h5: H5,
+  // @ts-expect-error
+  h6: H6,
 };
 
 const remarkPlugins = [remarkGfm];
