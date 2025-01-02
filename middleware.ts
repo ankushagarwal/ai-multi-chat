fca27f1a917c181cf21f11ed63a61e1e9d9a9b90
Ankush Agarwal <ankushagarwal11@gmail.com>
2025-01-02 08:21:59 -0800

    Update expiration time

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,32 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const host = request.headers.get('host');
-  const isLocalhost = host === 'localhost:3000';
+  const host = request.headers.get("host");
+  const isLocalhost = host === "localhost:3000";
 
   // Go to /authenticate to set the authenticated cookie
   // without the cookie, the user will be redirected to /404
   // this is a hack to prevent someone from accessing the app without logging in
-  if (request.nextUrl.pathname === '/authenticate') {
-    const response = NextResponse.redirect(new URL('/', request.url));
-    response.cookies.set('authenticated', 'true');
+  if (request.nextUrl.pathname === "/authenticate") {
+    const response = NextResponse.redirect(new URL("/", request.url));
+    const expirationDate = new Date();
+    expirationDate.setFullYear(expirationDate.getFullYear() + 10);
+
+    response.cookies.set("authenticated", "true", {
+      expires: expirationDate,
+    });
     return response;
   }
-  const authenticated = request.cookies.get('authenticated');
+  const authenticated = request.cookies.get("authenticated");
   if (!authenticated && !isLocalhost) {
-    return NextResponse.redirect(new URL('/404', request.url));
+    return NextResponse.redirect(new URL("/404", request.url));
   }
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/authenticate', '/settings'],
+  matcher: ["/", "/authenticate", "/settings"],
 };
